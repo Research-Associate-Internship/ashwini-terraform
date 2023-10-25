84a8242a3a97a04ca187fd2aad2cc90a88b03a31
ashwindh <109006799+ashwindh@users.noreply.github.com>
2023-10-25 11:00:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 provider "aws" {
     region = "us-east-1"  
 }
-resource "aws_instance" "first" {
-  ami           = "ami-03a6eaae9938c858c" # us-west-2
+resource "aws_instance" "Hashicorp-instance" {
+  ami           = "ami-0dbc3d7bc646e8516"
   instance_type = "t2.micro"
   subnet_id = "subnet-0860b49c7e7abaa7a"
+  key_name = "docker-jenkins"
   tags = {
-      Name = "TF-Instance"
+      Name = "HashiCorp-Instance"
   }
-}
-resource "aws_ec2_instance_state" "first" {
-  instance_id = aws_instance.first.id
-  state       = "stopped"
 }
