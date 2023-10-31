835dadaa575e7b7495e8d4248872cc51b6c3aa13
ashwindh <109006799+ashwindh@users.noreply.github.com>
2023-10-30 21:57:57 -0500

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_record" "rac3-hashicorp" {
-  zone_id = "Z03309881PN2JZ6GGLOJ8"
-  name    = "vault-devsecops-rac3.axle-interns.com"
-  type    = "A"
-  alias {
-    name                   = aws_lb.rac3-hashicorp.dns_name
-    zone_id                = aws_lb.rac3-hashicorp.zone_id
-    evaluate_target_health = true
-  }
-}
+#resource "aws_route53_record" "rac3-hashicorp" {
+  #zone_id = "Z03309881PN2JZ6GGLOJ8"
+  #name    = "vault-devsecops-rac3.axle-interns.com"
+  #type    = "A"
+  #alias {
+    #name                   = aws_lb.rac3-hashicorp.dns_name
+    #zone_id                = aws_lb.rac3-hashicorp.zone_id
+    #evaluate_target_health = true
+  #}
+#}
