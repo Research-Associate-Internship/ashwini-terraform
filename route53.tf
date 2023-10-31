8411c68911b5dde35129f280c469e79fbaf0aa37
ashwindh <109006799+ashwindh@users.noreply.github.com>
2023-10-30 21:54:45 -0500

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "rac3-hashicorp" {
   zone_id = "Z03309881PN2JZ6GGLOJ8"
-  name    = "vault-rac3.axle-interns.com"
+  name    = "vault-devsecops-rac3.axle-interns.com"
   type    = "A"
   alias {
     name                   = aws_lb.rac3-hashicorp.dns_name
